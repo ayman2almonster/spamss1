12d16d02574d2287aa9e09c1e868baa9111ec9bb
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-17 23:17:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "513443343724970005"; // ايدي السررفر
+var channel = "513447390146199552";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
